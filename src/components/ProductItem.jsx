fff416893403921fdc8dd02bbd79e762e750b21a
Xanim <xanim.imam@mail.ru>
2023-04-24 21:31:23 +0400

    fixing currency

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,20 @@
 const ProductItem = ({product}) => {
+    let truncatedDescription = product.description.slice(0, 100) + "...";
+    let formattedPrice = product.price.toLocaleString("en-US", {
+        style: "currency",
+        currency: product.currency,
+      });
+    
     return ( 
         <div className="product-card">
             <div className="product-image">
-                <img src={product.image} alt={product.description} />
+                <img src={product.image} alt={product.title} />
             </div>
-            <div className="product-info">{product.description}</div>
+            <div className="product-info">
+                <div className="product-title">{product.title}</div>
+                <div className="product-price">{formattedPrice}</div>
+                <div className="product-price">{truncatedDescription}</div>
+            </div>
         </div>
      );
 }

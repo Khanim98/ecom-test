ee13541ff0d881c0d0f863fbe410efc92e83562f
khanimpasha <khanim.pashayeva98@gmail.com>
2023-04-25 00:06:17 +0400

    debugging

--- src/App.js
+++ src/App.js
@@ -3,32 +3,31 @@ import ProductListing from "./components/ProductListing";
 import "./App.scss";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
+  const [products, setProducts] = useState(null);
+  // const [page, setPage] = useState(1);
   
-  // useEffect(()=> {
-  //   fetch(`https://my-json-server.typicode.com/khanim98/ecom-starfund/products`)
-  //   .then(response => response.json())
-  //   .then((data) => {
-  //     setProducts(data)
-  //     // if (data) {
-  //     //   setProducts((prevProducts) => [...prevProducts, ...data]);
-  //     // }
-  //   })
-  //   .catch((error) => console.log(error));
-  //   console.log(page)
-  // },[page]);
+  useEffect(()=> {
+    fetch(`https://my-json-server.typicode.com/khanimpasha/ecom-starfund/products`)
+    .then(response => response.json())
+    .then((data) => {
+      setProducts(data)
+      // if (data) {
+      //   setProducts((prevProducts) => [...prevProducts, ...data]);
+      // }
+    })
+    .catch((error) => console.log(error));
+  },[]);
 
-  function handleLoadMore() {
-    console.log("here")
-    setPage((prevPage) => prevPage + 1);
-  }
+  // function handleLoadMore() {
+  //   console.log("here")
+  //   setPage((prevPage) => prevPage + 1);
+  // }
 
   return (
     <div className="filtered-page">
       <div className="filtering-functions">Filtering functions</div>
-      <ProductListing products={products}/>
-      <button onClick={handleLoadMore}>Load More...</button>
+      {products && <ProductListing products={products}/>}
+      {/* <button onClick={handleLoadMore}>Load More...</button> */}
     </div>
   );
 }

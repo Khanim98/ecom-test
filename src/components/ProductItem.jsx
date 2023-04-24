033e4ca0e33be48b830b68cffe50a11b0bfbaaa1
Xanim <xanim.imam@mail.ru>
2023-04-24 21:08:41 +0400

    images changed in db

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,9 @@
 const ProductItem = ({product}) => {
     return ( 
         <div className="product-card">
-            <div className="product-image">image</div>
+            <div className="product-image">
+                <img src={product.image} alt={product.description} />
+            </div>
             <div className="product-info">{product.description}</div>
         </div>
      );
